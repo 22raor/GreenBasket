e36e64556e66c6d1f3879a23fd26ddf555067d08
Ananya Bhatia <ananya.bhatia@duke.edu>
2023-09-09 16:04:44 -0400

    added amazing api

--- notfrontend/main.py
+++ notfrontend/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://epic:epic@greenbasketdb.ilubdxm.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
 app = FastAPI()
 
 
@@ -12,3 +19,28 @@ async def root():
 @app.get("/eco_score/{id}")
 async def root(id):
     return {"eco_score": int(id) // 2}
+
+@app.get("/autocomplete/{product_query}")
+async def autocomplete(product_query: str):
+    return {searchEngine(product_query)}
+
+
+def searchEngine(toSearch):
+    # define pipeline
+    pipeline = [
+        {
+            '$search': {
+                'index': 'name_search', 
+                'autocomplete': {
+                    'query': toSearch, 
+                    'path': 'product_name'
+                }
+            }
+        }, {
+            '$limit': 5
+        }
+    ]
+    # run pipeline
+    result = client["products"]["foodfacts"].aggregate(pipeline)
+
+    return result
