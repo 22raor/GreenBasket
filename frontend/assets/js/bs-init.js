9cdbb1fd96516a2304a35455996b40ee0a130c4f
Rishi Rao <rvr6@duke.edu>
2023-09-10 02:38:34 -0400

    Rebuild frontend

--- frontend/assets/js/bs-init.js
+++ frontend/assets/js/bs-init.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var products = document.querySelectorAll('[data-bss-dynamic-product]');
+	var charts = document.querySelectorAll('[data-bss-chart]');
 
-	for (var product of products) {
-		var param = product.dataset.bssDynamicProductParam;
-		product.dataset.reflowProduct = new URL(location.href).searchParams.get(param)
+	for (var chart of charts) {
+		chart.chart = new Chart(chart, JSON.parse(chart.dataset.bssChart));
 	}
-
 }, false);
---
+++ frontend/assets/js/script.js
@@ -0,0 +1,68 @@
+let auth0Client = null;
+
+
+// const fetchAuthConfig = () => fetch("/auth_config.json");
+
+
+const configureClient = async () => {
+    const response = {
+    "domain": "dev-vehpzandb3knezrr.us.auth0.com",
+    "clientId": "C6c795SScHKdXHRtg4KM6yFJc3A5L3k8"
+  };
+    const config = response;
+  
+    auth0Client = await auth0.createAuth0Client({
+      domain: config.domain,
+      clientId: config.clientId
+    });
+  };
+
+  // ..
+
+  window.onload = async () => {
+
+    // .. code ommited for brevity
+    await configureClient();
+  
+    updateUI();
+  
+    const isAuthenticated = await auth0Client.isAuthenticated();
+  
+    if (isAuthenticated) {
+      // show the gated content
+      return;
+    }
+  
+    // NEW - check for the code and state parameters
+    const query = window.location.search;
+    if (query.includes("code=") && query.includes("state=")) {
+  
+      // Process the login state
+      await auth0Client.handleRedirectCallback();
+      
+      updateUI();
+  
+      // Use replaceState to redirect the user away and remove the querystring parameters
+      window.history.replaceState({}, document.title, "/");
+    }
+  };
+  
+  
+  // NEW
+  const updateUI = async () => {
+    const isAuthenticated = await auth0Client.isAuthenticated();
+  
+    //change text on button to signed in instead of log in
+
+    // document.getElementById("btn-logout").disabled = !isAuthenticated;
+    // document.getElementById("btn-login").disabled = isAuthenticated;
+  };
+
+
+  const login = async () => {
+    await auth0Client.loginWithRedirect({
+      authorizationParams: {
+        redirect_uri: "https://green-basket.vercel.app"
+      }
+    });
+  };
